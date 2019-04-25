7e1a87870a8d9bff9c5b251ca0a8a59beec87d97
qipengfei <qipengfei@dianxiaomi.com>
2019-04-25 21:18:24 +0800

    1111

--- annotion_test/src/main/java/com/my/Analysis/TestMapObejct.java
+++ annotion_test/src/main/java/com/my/Analysis/TestMapObejct.java
@@ -1,14 +1,37 @@
 package com.my.Analysis;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TestMapObejct {
     public static void main(String[] args) {
-        Map<String, Object> map = new HashMap<>();
-        Long authId = 1232321321L;
-        map.put("authId", authId);
+//        Map<String, Object> map = new HashMap<>();
+//        Long authId = 1232321321L;
+//        map.put("authId", authId);
+//
+//        System.out.println(String.valueOf((Long)map.get("authId")));
 
-        System.out.println(String.valueOf((Long)map.get("authId")));
+        Map<Integer, List<String>> map = new HashMap<>();
+        List<String> strList;
+        for (int i = 0; i < 8; i++) {
+            strList = new ArrayList<>();
+            strList.add("1111");
+            strList.add("eeee");
+            map.put(i, strList);
+        }
+        System.out.println(map);
+        Integer num;
+        for (Map.Entry<Integer, List<String>> integerListEntry : map.entrySet()) {
+            num = integerListEntry.getKey();
+            if (num == 2) {
+                integerListEntry.getValue().clear();
+            }
+            if (integerListEntry.getValue().size() == 0) {
+                map.remove(num);
+            }
+        }
+        System.out.println(map);
     }
 }
--- annotion_test/src/main/java/com/my/annotion/TestAnnotation_2.java
+++ annotion_test/src/main/java/com/my/annotion/TestAnnotation_2.java
@@ -5,6 +5,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 // target 如果不写则注解可以写在任何地方
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.TYPE})
+@Inherited
 @Documented
 public @interface TestAnnotation_2 {
 
